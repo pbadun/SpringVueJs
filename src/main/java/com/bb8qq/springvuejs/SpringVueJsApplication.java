fdc8b88234e7ca106ba95db86732c27ca6c2e410
pbadun <pbadun@gmail.com>
2022-07-26 09:19:40 +0300

    .

---
+++ src/main/java/com/bb8qq/springvuejs/SpringVueJsApplication.java
@@ -0,0 +1,13 @@
+package com.bb8qq.springvuejs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVueJsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringVueJsApplication.class, args);
+    }
+
+}
