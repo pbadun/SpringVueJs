385e4a1b77a5d54a21ed95047d25d0b43a7ccafd
pbadun <pbadun@gmail.com>
2022-07-26 13:33:00 +0300

    .

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,6 +1,8 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        message: 'Hello, Vue!'
+const HelloVueApp = {
+    data() {
+        return {
+            message: 'Hello Vue!!'
+        }
     }
-});
+}
+Vue.createApp(HelloVueApp).mount('#app');
